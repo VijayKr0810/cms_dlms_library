1a91ca4324bebc38c17ae8a5947a1a4b44c892b6
vijaykr0810 <vijayk@cmsgp.com>
2020-09-23 18:48:49 +0530

    23/Sep/2020:18:48:49 :: daily update

--- lib_git_update.sh
+++ lib_git_update.sh
@@ -2,7 +2,8 @@
 current_time=$(date +"%d/%b/%Y:%H:%M:%S")
 git add *
 git commit -m "$current_time :: daily update"
-git remote add origin  https://github.com/VijayKr0810/cms_dlms_library.git // Only One time required
+git remote add origin  https://github.com/VijayKr0810/cms_dlms_library.git 
+#// Only One time required
 git push --force origin master
 
-#read -rsp $'Repository updated.Press to exit...\n'
+read -rsp $'Repository updated.Press to exit...\n'
